1c111696731261d64e09844cddde729c120945ea
raunak-kumar <raunak.rd.rd@gmail.com>
2021-06-17 18:40:35 +0530

    hii

--- src/components/CurrencyConverter.js
+++ src/components/CurrencyConverter.js
@@ -6,7 +6,7 @@ export default function CurrencyConverter() {
     const [country2, setCountry2] = useState("INR")
     const [input1, setInput1] = useState(0)
     const [input2, setInput2] = useState(0)
-    const [exchangeRate, setExchangeRate] = useState(0.0)
+    const [exchangeRate, setExchangeRate] = useState(0)
     const [inputBox, setInputBox] = useState(0)
 
 
@@ -16,24 +16,19 @@ export default function CurrencyConverter() {
                 setData(x.rates)
                 setInput1(1)
                 setExchangeRate(x.rates[country2] / x.rates[country1])
-                setInput2(x.rates[country2] / x.rates[country1])
+                setInput2((x.rates[country2] / x.rates[country1]).toFixed(2))
             })
     }, [country1,country2]);
 
-    useEffect(()=>{
-        setExchangeRate(data[country1]/data[country2])
-        if(inputBox == 0){
-            if(input1 !=0 && input2 != 0){
-                setInput2(input1/exchangeRate)
-            }
+    useEffect(() => {
+        setExchangeRate(data[country1] / data[country2])
+        if (inputBox == 1) {
+            setInput2((input1 / exchangeRate).toFixed(2))
         }
-        else if(inputBox == 1){
-            setInput2(input1/exchangeRate)
+        else if (inputBox == 2) {
+            setInput1((input2 * exchangeRate).toFixed(2))
         }
-        else if(inputBox == 2){
-            setInput1(input2*exchangeRate)
-        }
-    },[inputBox,input1,input2])
+    }, [inputBox, input1, input2])
 
 
     function handleCountry1(e) {
@@ -45,14 +40,27 @@ export default function CurrencyConverter() {
     function handleInput1(e) {
         e.preventDefault();
         let input = e.target.value
-        setInput1(input)
-        setInputBox(1)
+        if (input < 0) {
+            setInput1(0)
+            setInputBox(1)
+        }
+        else {
+            setInput1(input)
+            setInputBox(1)
+        }
+
     }
     function handleInput2(e) {
         e.preventDefault();
         let input = e.target.value
-        setInput2(input)
-        setInputBox(2)
+        if (input < 0) {
+            setInput2(0)
+            setInputBox(2)
+        }
+        else {
+            setInput2(input)
+            setInputBox(2)
+        }
     }
 
 
@@ -66,7 +74,7 @@ export default function CurrencyConverter() {
                         return (<option key={item} value={item}>{item}</option>)
                     }
                 })}
-            </select><input type="number" onChange={e => { handleInput1(e) }} value={input1}></input>
+            </select><input type="number" min="0" onChange={e => { handleInput1(e) }} value={input1}></input>
             <br></br>
             <select onChange={e => { handleCountry2(e) }} value={country2}>
                 {Object.keys(data).map(item => {
@@ -74,7 +82,7 @@ export default function CurrencyConverter() {
                         return (<option key={item} value={item}>{item}</option>)
                     }
                 })}
-            </select><input type="number" onChange={e => { handleInput2(e) }} value={input2}></input>
+            </select><input type="number" min="0" onChange={e => { handleInput2(e) }} value={input2}></input>
         </div>
     )
 }
